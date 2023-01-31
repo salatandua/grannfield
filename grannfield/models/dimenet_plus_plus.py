e8caa076adbe14d066a93b49999e72112e6a0443
Salatan Duangdangchote <stpayu@live.com>
2023-01-30 20:52:30 -0500

    Update header

--- grannfield/models/dimenet_plus_plus.py
+++ grannfield/models/dimenet_plus_plus.py
@@ -9,12 +9,6 @@ salatandua/grannfield: https://github.com/salatandua/grannfield
 This code has been modified from the original version at
 Open-Catalyst-Project/ocp: https://github.com/Open-Catalyst-Project/ocp
 
----
-
-Copyright (c) Facebook, Inc. and its affiliates.
-
-This source code is licensed under the MIT license found in the
-LICENSE file in the root directory of this source tree.
 
 ---
 
